ae9cc3dd6c0d626ac52b0f5cfad8e9ebd1245215
A1n2F <alissonfferreira22@gmail.com>
2025-07-07 16:44:45 -0300

    Config do Prisma

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("Hello World")
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.get("/", async () => {
+    const habits = await prisma.habit.findMany()
+    return habits
+})
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log("HTTP Server running.")
+})
